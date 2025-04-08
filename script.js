eb128f71ace43ee3382cf5145e722fc13d76168c
bernardkrehula <krehula.bernard456@gmail.com>
2025-04-08 21:26:07 +0200

    Sum function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const deleteBtn = document.querySelector('.deleteBtn');
 const numbers = document.querySelectorAll('[data-number]');
 const numberOperation = document.querySelectorAll('[data-operation]');
 const currentNumberOnScreen = document.querySelector('.calculatorScreen h1');
+const totalNumberOnScreen = document.querySelector('.calculatorScreen h2');
 
 function numbersCreator() {
     let id = crypto.randomUUID();
@@ -13,63 +14,94 @@ function numbersCreator() {
     const getCurrentNumber = () => { return currentNumber };
     const toggleCurrentNumber = (value) => { currentNumber = value };
     const getTotalNumber = () => { return totalNumber };
-    const toggleTotalNumber = (value) => { };
+    const toggleTotalNumber = (value) => { totalNumber = value };
     
     return { getId, getCurrentNumber, toggleCurrentNumber, getTotalNumber, toggleTotalNumber }; 
 }
 
 function manageNumbers() {
     const numbers = [];
-    let currentNumber = 0;
-    let totalNumber = 0;
-    let clickedNumberString = '';
+    let clickedNumber = '';
+    const pushNumberInArray = () => numbers.push(numbersCreator());
 
-    const getClickedNumberForScreen = (number) => {
-        return clickedNumberString += number;
+    const getClickedNumber = (number) => {
+        return clickedNumber += number;
+    }
+    const pushClickedNumberInArray = () => {
+        numbers[0].toggleCurrentNumber(Number(clickedNumber));
+    }
+    const moveCurrentNumberToTotalNumber = () =>  {
+        clickedNumber = 0;
+        numbers[0].toggleTotalNumber(numbers[0].getCurrentNumber());
+        numbers[0].toggleCurrentNumber(0);
+    }
+    const returnCurrentNumber = () => {
+        return numbers[0].getCurrentNumber();
+    }
+    const returnTotalNumber = () => {
+        return numbers[0].getTotalNumber();
     }
     const sum = () => {
-        currentNumber += parseInt(clickedNumberString);
-        totalNumber += currentNumber;
-        return totalNumber;
+        let suma = returnCurrentNumber() + returnTotalNumber();
+        numbers[0].toggleCurrentNumber(suma);
+        
     }
     const subtraction = () => {
-
+        let subtract = returnCurrentNumber() - returnTotalNumber();
+        numbers[0].toggleCurrentNumber(subtract);
     }
     const multiplication = () => {
-
+        console.log(returnCurrentNumber())
+        console.log(returnTotalNumber())
+        let multiple = returnCurrentNumber() * returnTotalNumber();
+        numbers[0].toggleCurrentNumber(multiple);
     }
     const division = () => {
-
+        let divide = returnCurrentNumber() / returnTotalNumber()
+        numbers[0].toggleCurrentNumber(divide);
     }
-    const returnArray = () => { return numbers };
-    return { sum, subtraction, multiplication, division, returnArray, getClickedNumberForScreen };
+    const clearNumbers = () => {
+        numbers[0].toggleCurrentNumber(0);
+        numbers[0].toggleTotalNumber(0);
+    }
+    const returnArray = () => { return numbers[0].getTotalNumber() };
+    return { sum, subtraction, multiplication, division, returnArray, getClickedNumber, pushNumberInArray, pushClickedNumberInArray, moveCurrentNumberToTotalNumber, returnCurrentNumber, returnTotalNumber, clearNumbers };
 }
 
 const manager = manageNumbers();
+manager.pushNumberInArray();
 
 function displayNumbers(number) {
     currentNumberOnScreen.innerHTML = `${number}`;
 }
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        displayNumbers(manager.getClickedNumber(number.dataset.number))
+        manager.getClickedNumber(number.dataset.number)
+        manager.pushClickedNumberInArray();
+        displayNumbers(manager.returnCurrentNumber());
     })
 })
-numberOperation.forEach((operation) => {
-    operation.addEventListener('click', () => {
-        let calculatingOperationsString = operation.dataset.operation;
-      
-        if(calculatingOperationsString == '+'){
-            manager.sum()
+numberOperation.forEach((numOperation) => {
+    numOperation.addEventListener('click', () => {
+        let operation = numOperation.dataset.operation;
+
+        if(operation == '+'){
+            manager.sum();
         }
-        if(calculatingOperationsString == '*'){
-            console.log('radi')
+        if(operation == '-'){
+            manager.subtraction();
         }
-        if(calculatingOperationsString == '-'){
-            console.log('radi')
+        if(operation == '*'){
+            manager.multiplication();
         }
-        if(calculatingOperationsString == '/'){
-            console.log('radi')
+        if(operation == '/'){
+            manager.division();
         }
-    })
+        manager.moveCurrentNumberToTotalNumber();
+        currentNumberOnScreen.innerHTML = '';
+        totalNumberOnScreen.innerHTML = `${manager.returnArray()}`
+    });
+})
+clearBtn.addEventListener('click', () => {
+    manager.clearNumbers();
 })
