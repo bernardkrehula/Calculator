da982d3e1e42fa03df6f063c8b79564381b6b19c
bernardkrehula <krehula.bernard456@gmail.com>
2025-04-09 19:38:05 +0200

    Improved function operations

--- script.js
+++ script.js
@@ -1,107 +1,141 @@
 const clearBtn = document.querySelector('.clearBtn');
 const deleteBtn = document.querySelector('.deleteBtn');
+const equalsBtn = document.querySelector('[data-equals]');
 const numbers = document.querySelectorAll('[data-number]');
 const numberOperation = document.querySelectorAll('[data-operation]');
 const currentNumberOnScreen = document.querySelector('.calculatorScreen h1');
 const totalNumberOnScreen = document.querySelector('.calculatorScreen h2');
 
 function numbersCreator() {
-    let id = crypto.randomUUID();
     let currentNumber = 0;
     let totalNumber = 0;
 
-    const getId = () => { return id };
-    const getCurrentNumber = () => { return currentNumber };
+    const getCurrentNumber = () => currentNumber;
     const toggleCurrentNumber = (value) => { currentNumber = value };
-    const getTotalNumber = () => { return totalNumber };
+    const getTotalNumber = () => totalNumber;
     const toggleTotalNumber = (value) => { totalNumber = value };
-    
-    return { getId, getCurrentNumber, toggleCurrentNumber, getTotalNumber, toggleTotalNumber }; 
+
+    return { getCurrentNumber, toggleCurrentNumber, getTotalNumber, toggleTotalNumber }; 
 }
 
 function manageNumbers() {
     const numbers = [];
     let clickedNumber = '';
+    let currentOperation = '';
+
     const pushNumberInArray = () => numbers.push(numbersCreator());
 
     const getClickedNumber = (number) => {
-        return clickedNumber += number;
+        clickedNumber += number;
+        return clickedNumber;
     }
+
     const pushClickedNumberInArray = () => {
         numbers[0].toggleCurrentNumber(Number(clickedNumber));
     }
-    const moveCurrentNumberToTotalNumber = () =>  {
-        clickedNumber = 0;
+
+    const moveCurrentNumberToTotalNumber = () => {
         numbers[0].toggleTotalNumber(numbers[0].getCurrentNumber());
+        clickedNumber = '';
         numbers[0].toggleCurrentNumber(0);
     }
-    const returnCurrentNumber = () => {
-        return numbers[0].getCurrentNumber();
-    }
-    const returnTotalNumber = () => {
-        return numbers[0].getTotalNumber();
-    }
+
+    const returnCurrentNumber = () => numbers[0].getCurrentNumber();
+    const returnTotalNumber = () => numbers[0].getTotalNumber();
+    const returnArray = () => numbers[0];
+
+    const setClicked = (val) => { clickedNumber = val };
+    const getClicked = () => clickedNumber;
+
+    const setOperation = (op) => { currentOperation = op };
+    const getOperation = () => currentOperation;
+
     const sum = () => {
-        let suma = returnCurrentNumber() + returnTotalNumber();
-        numbers[0].toggleCurrentNumber(suma);
-        
+        numbers[0].toggleCurrentNumber(returnTotalNumber() + returnCurrentNumber());
     }
+
     const subtraction = () => {
-        let subtract = returnCurrentNumber() - returnTotalNumber();
-        numbers[0].toggleCurrentNumber(subtract);
+        numbers[0].toggleCurrentNumber(returnTotalNumber() - returnCurrentNumber());
     }
+
     const multiplication = () => {
-        console.log(returnCurrentNumber())
-        console.log(returnTotalNumber())
-        let multiple = returnCurrentNumber() * returnTotalNumber();
-        numbers[0].toggleCurrentNumber(multiple);
+        numbers[0].toggleCurrentNumber(returnTotalNumber() * returnCurrentNumber());
     }
+
     const division = () => {
-        let divide = returnCurrentNumber() / returnTotalNumber()
-        numbers[0].toggleCurrentNumber(divide);
+        numbers[0].toggleCurrentNumber(returnTotalNumber() / returnCurrentNumber());
     }
+
     const clearNumbers = () => {
         numbers[0].toggleCurrentNumber(0);
         numbers[0].toggleTotalNumber(0);
+        clickedNumber = '';
+        currentOperation = '';
     }
-    const returnArray = () => { return numbers[0].getTotalNumber() };
-    return { sum, subtraction, multiplication, division, returnArray, getClickedNumber, pushNumberInArray, pushClickedNumberInArray, moveCurrentNumberToTotalNumber, returnCurrentNumber, returnTotalNumber, clearNumbers };
+
+    return { sum, subtraction, multiplication, division, returnArray, getClickedNumber, pushNumberInArray, pushClickedNumberInArray, moveCurrentNumberToTotalNumber, returnCurrentNumber, returnTotalNumber, clearNumbers, getClicked, setClicked, setOperation, getOperation};
 }
 
 const manager = manageNumbers();
 manager.pushNumberInArray();
 
 function displayNumbers(number) {
-    currentNumberOnScreen.innerHTML = `${number}`;
+    currentNumberOnScreen.innerHTML = number.getCurrentNumber();
 }
+
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        manager.getClickedNumber(number.dataset.number)
+        const getArrayNumber = manager.returnArray();
+
+        manager.getClickedNumber(number.dataset.number);
         manager.pushClickedNumberInArray();
-        displayNumbers(manager.returnCurrentNumber());
-    })
-})
+        displayNumbers(getArrayNumber);
+    });
+});
+
 numberOperation.forEach((numOperation) => {
     numOperation.addEventListener('click', () => {
-        let operation = numOperation.dataset.operation;
+        const operation = numOperation.dataset.operation;
+        const getArrayNumber = manager.returnArray();
 
-        if(operation == '+'){
-            manager.sum();
-        }
-        if(operation == '-'){
-            manager.subtraction();
-        }
-        if(operation == '*'){
-            manager.multiplication();
-        }
-        if(operation == '/'){
-            manager.division();
-        }
+        manager.setOperation(operation);
         manager.moveCurrentNumberToTotalNumber();
-        currentNumberOnScreen.innerHTML = '';
-        totalNumberOnScreen.innerHTML = `${manager.returnArray()}`
+
+        totalNumberOnScreen.innerHTML = `${getArrayNumber.getTotalNumber()} ${operation}`;
+        displayNumbers(getArrayNumber);
     });
-})
+});
+
+equalsBtn.addEventListener('click', () => {
+    const getArrayNumber = manager.returnArray();
+    const operation = manager.getOperation();
+
+    if (operation === '+') manager.sum();
+    if (operation === '-') manager.subtraction();
+    if (operation === '*') manager.multiplication();
+    if (operation === '/') manager.division();
+
+    displayNumbers(getArrayNumber);
+    totalNumberOnScreen.innerHTML = '';
+    manager.setOperation('');
+});
+
 clearBtn.addEventListener('click', () => {
     manager.clearNumbers();
-})
+    currentNumberOnScreen.innerHTML = '0';
+    totalNumberOnScreen.innerHTML = '';
+});
+
+deleteBtn.addEventListener('click', () => {
+    let value = manager.getClicked();
+    value = value.slice(0, -1);
+    manager.setClicked(value);
+
+    if (value === '') {
+        manager.returnArray().toggleCurrentNumber(0);
+    } else {
+        manager.returnArray().toggleCurrentNumber(Number(value));
+    }
+
+    displayNumbers(manager.returnArray());
+});
