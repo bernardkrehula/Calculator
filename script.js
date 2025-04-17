38c96e447520e68488a01ee994afc7e430e51ea1
bernardkrehula <krehula.bernard456@gmail.com>
2025-04-17 20:15:29 +0200

    Improve decimal operation function

--- script.js
+++ script.js
@@ -30,49 +30,18 @@ const creator = numbersCreator();
 function manageNumbers() { 
     let clickedNumber = '';
 
-    const getClickedNumber = (number) => {
+    const setClickedNumber = (number) => {
         clickedNumber += number;
         return clickedNumber;
     }
-    const convertStringToNumber = () => {
-        clickedNumber = Number(clickedNumber);
+    const pushNumberToFirstNumber = () => {
+        creator.toggleFirstNumber(Number(clickedNumber))
     }
 
-    const setCurrentNumberToZero = () => {
+    const getClickedNumber = () => clickedNumber;
+
+    const setClickedNumberToZero = () => {
         clickedNumber = '';
-        creator.toggleFirstNumber(0);
-    }
-
-    const returnFirstNumber = () => creator.getFirstNumber();
-    const returnSecondNumber = () => creator.getSecondNumber();
-    const returnResultNumber = () => creator.getResultNumber();
-
-    const setClicked = (val) => { clickedNumber = val };
-    const getClicked = () => clickedNumber;
-
-    const getOperation = () => { return operation };
-    const setOperation = (value) => { operation = value };
-
-    const moveCurrentNumberToResultNumber = () => { numbers[0].toggleResultNumber(returnCurrentNumber()) }
-
-    const sum = () => {
-        let suma = returnResultNumber() + returnCurrentNumber();
-        numbers[0].toggleResultNumber(suma);
-    }
-
-    const subtraction = () => {
-        let subtract = returnResultNumber() - returnCurrentNumber();
-        numbers[0].toggleResultNumber(subtract);
-    }
-
-    const multiplication = () => {
-        let multiplicate = returnResultNumber() * returnCurrentNumber();
-        numbers[0].toggleResultNumber(multiplicate);
-    }
-
-    const division = () => {
-        let divide = returnResultNumber() / returnCurrentNumber();
-        numbers[0].toggleResultNumber(divide);
     }
 
     const clearNumbers = () => {
@@ -81,15 +50,10 @@ function manageNumbers() {
         clickedNumber = '';
     }
 
-    return { convertStringToNumber, returnFirstNumber, returnResultNumber, returnSecondNumber, moveCurrentNumberToResultNumber, getOperation, setOperation, returnResultNumber, sum, subtraction, multiplication, division, getClickedNumber, setCurrentNumberToZero, clearNumbers, getClicked, setClicked };
+    return { pushNumberToFirstNumber, getClickedNumber, setClickedNumber, setClickedNumberToZero, clearNumbers };
 }
 
 const manager = manageNumbers();
-
-let firstNumber = '';
-let secondNumber;
-let result = 0;
-let operation;
 
 function calculate(a, b, operation) {
     switch (operation) {
@@ -114,41 +78,41 @@ numbers.forEach((number) => {
     number.addEventListener('click', () => {
        let num = number.dataset.number;
 
-       currentNumberOnScreen.innerHTML = `${firstNumber}`;
-       manager.getClickedNumber(num)
+       manager.setClickedNumber(num);
+       currentNumberOnScreen.innerHTML = `${manager.getClickedNumber()}`;
     });
 });
 
 numberOperation.forEach((numOperation) => {
     numOperation.addEventListener('click', () => {
         let operate = numOperation.dataset.operation;
-        creator.setOperation(operate);
-
-        if (manager.returnFirstNumber() !== '') {
-            firstNumber = Number(firstNumber);
+        manager.pushNumberToFirstNumber();
+      
+        if (creator.getFirstNumber() !== '') {
             currentNumberOnScreen.innerHTML = '';
           
-            if (manager.returnSecondNumber() === undefined) {
-                manager.toggleSecondNumber(firstNumber);
-                manager.toggleFirstNumber('');
-                manager.setOperation(operate);
+            if (creator.getSecondNumber() === undefined) {
+                creator.toggleSecondNumber(creator.getFirstNumber());
+                creator.toggleFirstNumber('');
+                creator.setOperation(operate);
             } 
       
             else {
-                manager.toggleResultNumber(calculate(firstNumber, secondNumber, operation)) ;
-                manager.toggleSecondNumber(result);
-                manager.toggleFirstNumber('');
-                manager.setOperation(operate);
+                creator.toggleResultNumber(calculate(creator.getFirstNumber(), creator.getSecondNumber(), creator.getOperation())) ;
+                creator.toggleSecondNumber(creator.getResultNumber());
+                creator.toggleFirstNumber('');
+                creator.setOperation(operate);
                 
-                totalNumberOnScreen.innerHTML = `${manager.getSecondNumber()}`;
+                totalNumberOnScreen.innerHTML = `${creator.getSecondNumber()}`;
             }
         }
-        totalNumberOnScreen.innerHTML = `${secondNumber}` + `${operate}`;
+        manager.setClickedNumberToZero();
+        totalNumberOnScreen.innerHTML = `${creator.getSecondNumber()}` + `${creator.getOperation()}`;
     });
 });
 decimalOperation.addEventListener('click', () => {
-    firstNumber = firstNumber + '.';
-    currentNumberOnScreen.innerHTML = firstNumber;
+    manager.setClickedNumber('.');
+    currentNumberOnScreen.innerHTML = manager.getClickedNumber();
 })
 equalsBtn.addEventListener('click', () => {
     result = calculate(Number(firstNumber), secondNumber, operation);
